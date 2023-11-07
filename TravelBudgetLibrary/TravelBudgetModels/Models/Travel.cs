ad19277c57aa6d0e1277749738a39054d9576c5b
Mariusz Klatkowski <mariusz.klatkowski95@gmail.com>
2023-11-07 10:12:07 +0100

    Adjusting css Create view style and tables in the application.

--- TravelBudgetLibrary/TravelBudgetModels/Models/Travel.cs
+++ TravelBudgetLibrary/TravelBudgetModels/Models/Travel.cs
@@ -1,9 +1,17 @@
-﻿namespace TravelBudgetModels.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TravelBudgetModels.Models
 {
     public class Travel
     {
         public int Id { get; set; }
+        [Display(Name = "Date")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm}", ApplyFormatInEditMode = true)]
         public DateTime StartingDate { get; set; } = new DateTime();
+        [Display(Name = "Date")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm}", ApplyFormatInEditMode = true)]
         public DateTime FinishDate { get; set; } = new DateTime();
         public string? Name { get; set; }
         public string? Description { get; set; }
