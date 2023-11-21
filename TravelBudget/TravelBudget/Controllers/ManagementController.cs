ec0a94fe53e1a52c49e151e787658cc5747aa227
Mariusz Klatkowski <mariusz.klatkowski95@gmail.com>
2023-11-21 23:29:19 +0100

    Create background for menagement section.

---
+++ TravelBudget/TravelBudget/Controllers/ManagementController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TravelBudget.Controllers
+{
+    public class ManagementController : Controller
+    {
+        [HttpGet]
+        public IActionResult Zone()
+        {
+            return View();
+        }
+    }
+}
---
+++ TravelBudget/TravelBudget/Models/ManagementViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TravelBudgetContactContext.Response.DTO;
+using TravelBudgetModels.Models;
+
+namespace TravelBudget.Models
+{
+    public class ManagementViewModel
+    {
+        [Display(Name = "Add New Category:")]
+        public IEnumerable<Category> Categories { get; set; }
+        public Category Category { get; set; }
+    }
+}
