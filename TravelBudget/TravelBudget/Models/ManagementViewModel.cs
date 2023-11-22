6898ca8b8b55cdff0d47fc922466c0eb871046c1
Mariusz Klatkowski <mariusz.klatkowski95@gmail.com>
2023-11-22 17:32:25 +0100

    Management view adjusted.

--- TravelBudget/TravelBudget/Models/ManagementViewModel.cs
+++ TravelBudget/TravelBudget/Models/ManagementViewModel.cs
@@ -6,7 +6,7 @@ namespace TravelBudget.Models
 {
     public class ManagementViewModel
     {
-        [Display(Name = "Add New Category:")]
+        [Display(Name = "Create new category:")]
         public IEnumerable<Category> Categories { get; set; }
         public Category Category { get; set; }
     }
