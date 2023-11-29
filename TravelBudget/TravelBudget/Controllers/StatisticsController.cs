5c6f88ef84f1ec5f5ae189d5ffa5c15fa43b3d80
Mariusz Klatkowski <mariusz.klatkowski95@gmail.com>
2023-11-29 17:36:58 +0100

    Adding Statistics framework.

---
+++ TravelBudget/TravelBudget/Controllers/StatisticsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TravelBudgetDBContact.Repositories.Interfaces;
+
+namespace TravelBudget.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        private readonly IExpenseRepository _expenseRepository;
+        public StatisticsController(IExpenseRepository expenseRepository)
+        {
+            _expenseRepository = expenseRepository;
+        }
+        public IActionResult Statistics()
+        {
+            return View();
+        }
+    }
+}
