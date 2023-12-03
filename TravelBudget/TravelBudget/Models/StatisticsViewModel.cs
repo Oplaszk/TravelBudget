9b451d054f10a9babbc2d2d3ed2694f44f070690
Mariusz Klatkowski <mariusz.klatkowski95@gmail.com>
2023-12-03 09:50:50 +0100

    Adding partial view to Travel views.

---
+++ TravelBudget/TravelBudget/Models/StatisticsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using TravelBudgetDBModels.Models;
+
+namespace TravelBudget.Models
+{
+    public class StatisticsViewModel
+    {
+        public IEnumerable<Travel> Travels { get; set; }
+        public IEnumerable<Expense> Expenses { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+    }
+}
---
+++ TravelBudgetLibrary/TravelBudgetContactContext/Repositories/StatisticsRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelBudgetDBContact.Repositories
+{
+    public class StatisticsRepository : BaseRepository
+    {
+        private readonly ILogger<StatisticsRepository> _logger;
+        public StatisticsRepository(DBContact db, ILogger<StatisticsRepository> logger) : base(db)
+        {
+            _logger = logger;
+        }
+
+
+    }
+}
